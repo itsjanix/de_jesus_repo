115077c8b1c8583e830c607e1253d64946f227aa
Angel Janica Marie De Jesus <angeljanicamarie.dejesus@students.isatu.edu.ph>
2022-10-10 03:29:45 +0000

    Commit

---
+++ Rworksheet_De_Jesus_#2/Worksheet-2.R
@@ -0,0 +1,194 @@
+---
+  title: "Worksheet-2"
+author: "Angel Janica Marie De Jesus"
+date: "2022-10-10"
+output: pdf_document
+---
+  
+  Instructions:
+•	Use RStudio or the RStudio Cloud accomplish this worksheet. + Save the R script as RWorksheet_lastname#2.R.
+•	Create your own GitHub repository and push the R script as well as this pdf worksheet to your own repo.
+Accomplish this worksheet by answering the questions being asked and writing the code manually.
+Using Vectors
+
+#1. Create a vector using : operator
+#a.	Sequence from -5 to 5. Write the R code and its output. 
+
+x <- -5:5
+x
+
+Describe its output:
+  The output is counting what is within negative 5 and positive 5 and the their value is from -5 to 5.
+
+
+#b.	x <- 1:7. What will be the value of x?
+  
+
+x <- 1:7
+x
+
+
+#2.* Create a vector using seq() function
+a. seq(1, 3, by=0.2) # specify step size
+Write the R code and its output. 
+
+
+seq(1, 3, by=0.2)
+
+OUTPUT:
+  [1] 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0
+
+Describe the output:
+  The output is counting by 0.2 decimal starting from 1 to 3.
+
+
+#3. A factory has a census of its workers. There are 50 workers in total. The following list shows their ages: 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35,
+24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,
+18.
+
+ages <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35,24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,18)
+
+
+#a.	Access 3rd element, what is the value?
+  
+ages [3]
+
+VALUE: [1] 22
+
+#b.	Access 2nd and 4th element, what are the values?
+  
+
+ages [2]
+ages[4]
+
+VALUE:
+  [1] 28
+[1] 36
+
+#c.	Access all but the 1st element is not included. Write the R code and its output.
+
+ages[-1]
+
+
+OUTPUT: 
+  [1] 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17 37 43 53
+[28] 41 51 35 24 33 41 53 40 18 44 38 41 48 27 39 19 30 61 54 58 26 18
+
+
+#4.	*Create a vector x <- c("first"=3, "second"=0, "third"=9). Then named the vector, names(x).
+
+#a.	Print the results. Then access x[c("first", "third")]. 
+
+x[c("first", "third")]
+
+
+#Describe the output: The output printed are 3 and 9 because they are the numbers assigned in first and third. 0 is not printed because it was not asked to print together with first and third.
+
+#b.	Write the code and its output.
+
+x <- c("first"=3, "second"=0, "third"=9)
+x[c("first", "third")]
+
+output: 
+  first third 
+3     9 
+5    Create a sequence x from -3:2.
+a.	Modify 2nd element and change it to 0;x[2] <- 0 x
+
+x <- seq(-3:2)
+x[2] <- 0
+x
+
+Describe the output: 2nd number "2" is changed to "0" because of declaring 0 to replace the 2nd position which is originally "2".
+
+
+#b.	Write the code and its output.
+
+x <- seq(-3:2)
+x[2] <- 0
+x
+
+OUTPUT: [1] 1 0 3 4 5 6
+
+
+#6.	*The following data shows the diesel fuel purchased by Mr. Cruz.
+Month	Jan	Feb	March	Apr	May	June
+Price per liter (PhP)	52.50	57.25	60.00	65.00	74.25	54.00
+Purchase–quantity(Liters)	25	30	40	50	10	45
+#a.	Create a data frame for month, price per liter (php) and purchase-quantity (liter).Write the codes.
+data <- data.frame (Month =c("price_per_liter_php", "purchase_quantity_liter"),
+                    Jan = c("52.50","25"),Feb = c("57.25","30"),March = c("60.00","40"),April = c("65.00","50"),
+                    May = c("74.25","10"),June = c("54.00","45"))
+data
+
+)
+
+#b.	What is the average fuel expenditure of Mr. Cruz from Jan to June? 
+  
+price_per_liter_php <- c(52.50, 57.25, 60.00, 65.00, 74.25, 54.00)
+price_per_liter_php
+
+
+purchase_quantity_liter <- c(25, 30, 40, 50, 10, 45)
+purchase_quantity_liter
+
+
+Note: Use weighted.mean(liter, purchase)
+weighted.mean(price_per_liter_php, purchase_quantity_liter )
+
+#7. R has actually lots of built-in datasets. For example, the rivers data “gives the lengths 
+(in miles) of 141“major” rivers in North America, as compiled by the US Geological Survey”. 
+
+#a. Type “rivers” in your R console. 
+Create a vector data with 7 elements, containing the number of elements (length) in rivers, their
+sum (sum), mean (mean), median (median), variance (var) standard deviation (sd), minimum (min) and
+maximum (max).
+
+Riv <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+         sd(rivers), min(rivers), max(rivers))
+Riv
+
+
+#b. What are the results? 
+  
+  The result printed are the river's length, sum, mean, median, variance, standard deviation, minimum and maximum data.
+
+#8. The table below gives the 25 most powerful celebrities and their annual pay as ranked by 
+the editions of Forbes magazine and as listed on the Forbes.com website.
+
+#a. Create vectors according to the above table. 
+Write the codes.
+
+Pow <- 1:25
+Celeb <- c("Tom Cruise", "Rolling Stones", "Oprah Winfrey", "U2", "Tiger Woods",
+"Steven Spielberg", "Howard Stern", "50 Cent", "Cast of the sopranos","Dan Brown",
+"Bruce Springsteen", "Donald Trump", "Muhammad Ali", "Paul McCartney","George Lucas",
+"Elton John", "David Letterman", "Phil Mickelson", "J.K Rowling", "Bradd Pitt",
+"Peter Jackson", "Dr. Phil McGraw", "Jay Lenon", "Celine Dion", "Kobe Bryant")
+
+pay <- c(67, 90, 225, 110, 90, 332, 302, 41, 52, 88, 55, 44, 55, 40,
+         233, 34, 40, 47, 75, 25, 39, 45, 32, 40, 31)
+
+
+
+Drank <- data.frame(Pow, Celeb, pay)
+Drank
+
+
+#b. Modify the power ranking and pay of J.K. Rowling. Change power ranking to 15 and pay to 90.
+
+Pow [19]<-15
+Pow
+
+
+Pay [19] <-90
+Pay
+
+
+
+Magazine_Rank <- data.frame(Pow, Celeb, pay)
+Magazine_Rank
+
+
+#c. Interpret the data
+All of the data were changed by declaring the data frame's object and using brackets[]. Obtaining the rank number via the vector name, where the values need to be. Finally, to access the modified data, declare the object name again. JK Rowling's rank was reduced from 19 to 15, and her annual salary increased from 75 to 90.
