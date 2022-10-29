46430fcb3581d965ecb994b19d8905bd02a16bcd
itsjanix <angeljanicamarie.dejesus@students.isatu.edu.ph>
2022-10-29 05:40:32 +0000

    ADDED FILE

---
+++ Rworksheet_De_Jesus_#3/Worksheet3a.R
@@ -0,0 +1,196 @@
+install.packages("devtools")
+install.packages("pdflatex")
+tinytex::install_tinytex()
+tinytex::tlmgr_update()
+tinytex::reinstall_tinytex()
+tinytex::install_tinytex()
+library(tinytex)
+tinytex::pdflatex
+
+
+---
+  title: "WORKSHEET-3A"
+author: "Angel Janica Marie De Jesus"
+date: "2022-10-28"
+output: pdf_document
+---
+#Worksheet-3a in R
+#Worksheet for R Programming
+Instructions:
+#  •	Use RStudio or the RStudio Cloud accomplish this worksheet.
+#•	Save the R script as RWorksheet_lastname#3a.R.
+#•	On your own GitHub repository, push the R script, the Rmd file, as well as this pdf worksheet to the repo you have created before.
+#•	Do not forget to comment your Git repo on our VLE
+#•	Accomplish this worksheet by answering the questions being asked and writing the code manually.
+Reminder
+#•	To create a chunk of codes, you need to indicate this structure
+
+#Figure 1: R Chunk
+#•	You can add chunk options if you want, like:
+# –	echo: Whether to echo the source code in the output document (someone may not prefer reading your smart source code but only results).
+
+#Using Vectors
+#1. There is a built-in vector LETTERS contains the uppercase letters of the alphabet
+#and letters which contains the lowercase letters of the alphabet.
+LETTERS
+## [1] "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S"
+## [20] "T" "U" "V" "W" "X" "Y" "Z"
+letters
+## [1] "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s"
+## [20] "t" "u" "v" "w" "x" "y" "z"
+#Based on the above vector LETTERS:
+ # a. You need to produce a vector that contains the first 11 letters.
+
+LETTERS [1:11]
+
+#b. Produce a vector that contains the odd numbered letters.
+
+oddletters <-LETTERS[1:26 %% 2 ==1]
+oddletters
+
+#c. Produce a vector that contains the vowels
+
+vowels <- LETTERS [c(1,5,9,15,21)]
+vowels
+
+
+#Based on the above vector letters: d. Produce a vector that contains the last 5 lowercase letters.
+
+fiveletters <- letters[21:26]
+fiveletters
+
+#e. Produce a vector that contains letters between 15 to 24 letters in lowercase.
+
+
+fiveletters <- letters[15:24]
+fiveletters
+
+
+#2. Create a vector with the average temperatures in April for Tuguegarao City, Manila, Iloilo City, Tacloban,
+#Samal Island, and Davao City. The average temperatures in Celcius are 42, 39, 34, 34, 30, and 27 degrees.
+
+#a. What is the R code and its result for creating a character vector for the city/town of Tuguegarao City,
+#Manila, Iloilo City, Tacloban, Samal Island, and Davao City? Name the object as city. The names
+#should follow the same order as in the instruction.
+
+citytown <-c("Tuguegarao City", "Manila", "Iloilo City", "Tacloban", "Samal Island", "Davao City")
+citytown
+
+#b. The average temperatures in Celcius are 42, 39, 34, 34, 30, and 27 degrees. Name the object as temp.
+#Write the R code and its output. Numbers should also follow what is in the instruction.
+
+temp <-c(42, 39, 34, 34, 30, 27)
+temp
+
+#c. Associate the temperature temp with the city by using names() function. What is the R code and its
+#result.
+
+names(temp) <- citytown
+temp
+
+#d. From the answer in d, what is the content of index 5 and index 6? What is its R code?
+
+names_index56 <- temp[5:6]
+names_index56
+
+#Using Matrices
+#2. Create a matrix of one to eight and eleven to fourteen with four columns and three # rows.
+
+#a. What will be the R code for the #2 question and its result?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+#b. Multiply the matrix by two. What is its R code and its result?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+result <- tab1 * 2
+result
+
+#c. What is the content of row 2? What is its R code?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+content <- c(tab1[2,1], tab1[2,2], tab1[2,3], tab1[2,4])
+content
+
+#d. What will be the R code if you want to display the column 3 and column 4 in row 1 and row 2? What
+#is its output?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+content <- c(tab1[1,3], tab1[1,4], tab1[2,3], tab1[2,4])
+content
+
+#e. What is the R code is you want to display only the columns in 2 and 3, row 3? What is its output?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+content <- c(tab1[3,2], tab1[3,3])
+content
+
+#f. What is the R code is you want to display only the columns 4? What is its output?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+content <- c(tab1[1,4], tab1[2,4], tab1[3,4])
+content
+
+#g. Name the rows as isa, dalawa, tatlo and columns as uno, dos, tres, quatro for the matrix that was
+#created in b.‘. What is its R code and corresponding output?
+
+tab1 <- matrix(c(1:8, 11:14), nrow = 3, ncol = 4)
+tab1
+
+dimnames(tab1) <- list(c("isa", "dalawa", "tatlo"),c("uno", "dos", "tres", "quatro"))
+tab1
+
+#h. From the original matrix you have created in a, reshape the matrix by assigning a new dimension with
+#dim(). New dimensions should have 2 columns and 6 rows. What will be the R code and its output?
+
+tab1 <- matrix(1:8, 11:14, nrow = 3, ncol = 4)
+
+tab1
+
+newtab <- c(1,2,3,4,5,6,7,8,11,12,13,14)
+newtab
+
+x <- matrix(newtab, nrow = 6, ncol = 2)
+x
+
+dim(x)
+
+
+#Using Arrays
+#3. An array contains 1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1
+
+#a. Create an array for the above numeric values. Each values will
+#be repeated twice
+#What will be the R code if you are to create a three-dimensional
+#array with 4 columns and 2 rows. What will be its output?
+  
+a <- c(1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1)
+a
+
+
+x <- array(rep(a, 2), dim = c(2,4,3))
+x
+
+#b. How many dimensions do your array have? # It has 3 dimensions.
+#c. Name the rows as lowercase letters and columns as uppercase letters starting from the A. The array
+#names should be “1st-Dimensional Array”, “2nd-Dimentional Array”, and “3rd-Dimensional Array”.
+#What will be the R codes and its output?
+  
+a <- c(1, 2, 3, 6, 7, 8, 9, 0, 3, 4, 5, 1)
+a
+
+
+x <- array(rep(a, 2), dim = c(2,4,3))
+x
+
+dimnames(x) <- list( letters[1:2], LETTERS[1:4], c("1st-Dimensional Array", "2nd-Dimensional Array", "3rd-Dimentional Array"))
+x
